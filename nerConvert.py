1e2738a2cc4b6da89a4c817713dc380ba6fec346
Saša Petalinkar <sasa5linkar@gmail.com>
2023-11-09 15:19:44 +0100

    seapred lat from cyr

--- nerConvert.py
+++ nerConvert.py
@@ -59,71 +59,43 @@ def create_docbin(data, nlp):
             if span is not None:
                 ents.append(span)
         doc.ents = ents
+        doc_bin.add(doc)
 
-        # Split the doc into smaller docs based on punctuation
-        split_points = [i for i, token in enumerate(doc) if token.text in {'.', '!', '?'} and doc[min(i + 1, len(doc) - 1)].text != '.']
-        start = 0
-        for end in split_points:
-            block_span = doc[start:end + 1]
-            block_doc = nlp.make_doc(block_span.text)
-            block_doc.ents = [Span(block_doc, ent.start - start, ent.end - start, label=ent.label_) for ent in block_span.ents if ent.start >= start and ent.end <= end]
-            doc_bin.add(block_doc)
-            start = end + 1
-        block_span = doc[start:]  # Add the last block
-        block_doc = nlp.make_doc(block_span.text)
-        block_doc.ents = [Span(block_doc, ent.start - start, ent.end - start, label=ent.label_) for ent in block_span.ents if ent.start >= start and ent.end <= len(doc)]
-        doc_bin.add(block_doc)
     return doc_bin
 
-@Language.component("set_sentence_boundaries")
-def set_sentence_boundaries(doc):
-    for token in doc:
-        if token.i == 0:
-            token.is_sent_start = True
-        elif token.text in [".", "!", "?"]:
-            if token.i + 1 < len(doc) and not doc[token.i + 1].is_punct:
-                doc[token.i + 1].is_sent_start = True
-    return doc
-# Path to the directory containing BRAT annotated files
-dir_path = NER_DIR
+def main():
+    # Path to the directory containing BRAT annotated files
+    dir_path = NER_DIR
 
-# List all .txt files in the directory
-all_txt_files = [f for f in os.listdir(dir_path) if f.endswith('.txt')]
+    # List all .txt files in the directory
+    all_txt_files = [f for f in os.listdir(dir_path) if f.endswith('.txt')]
 
-# Convert the entire dataset using the order-based approach
-all_ordered_converted_data = {}
-for txt_file in all_txt_files:
-    ann_file = txt_file.replace(".txt", ".ann")
-    if ann_file in os.listdir(dir_path):
-        converted_data = ordered_search_based_brat_to_spacy(os.path.join(dir_path, txt_file), os.path.join(dir_path, ann_file))
-        all_ordered_converted_data[txt_file] = converted_data
+    # Convert the entire dataset using the order-based approach
+    all_ordered_converted_data = {}
+    for txt_file in all_txt_files:
+        ann_file = txt_file.replace(".txt", ".ann")
+        if ann_file in os.listdir(dir_path):
+            converted_data = ordered_search_based_brat_to_spacy(os.path.join(dir_path, txt_file), os.path.join(dir_path, ann_file))
+            all_ordered_converted_data[txt_file] = converted_data
 
-# Split the data into training and validation sets
-train_files, valid_files = train_test_split(list(all_ordered_converted_data.keys()), test_size=0.2, random_state=42)
+    # Combine all data
+    all_data = [all_ordered_converted_data[file] for file in all_txt_files]
 
-train_data = [all_ordered_converted_data[file] for file in train_files]
-valid_data = [all_ordered_converted_data[file] for file in valid_files]
+    # Create a blank Serbian model
+    nlp = spacy.load(os.path.join(ROOT_DIR, "model10", "model-best"))
 
-# Create a blank Serbian model
-nlp = spacy.blank("sr")
+    all_doc_bin = create_docbin(all_data, nlp)
 
-# Add the custom component to the pipeline that sets sentence boundaries
-nlp.add_pipe("set_sentence_boundaries")
+    # Save the binary file
+    ouputName = "SrpELTeC-gold"
 
-train_doc_bin = create_docbin(train_data, nlp)
-valid_doc_bin = create_docbin(valid_data, nlp)
+    all_bin_path = os.path.join(CORPUS_DIR, ouputName + ".spacy")
 
-# Save the binary files
+    all_doc_bin.to_disk(all_bin_path)
 
-ouputName = "SrpELTeC-gold"
+    print(f"All data saved to: {all_bin_path}")
 
-train_bin_path = os.path.join(CORPUS_DIR, ouputName + "-train.spacy")
-valid_bin_path = os.path.join(CORPUS_DIR, ouputName + "-dev.spacy")
-
-train_doc_bin.to_disk(train_bin_path)
-valid_doc_bin.to_disk(valid_bin_path)
-
-print(f"Training data saved to: {train_bin_path}")
-print(f"Validation data saved to: {valid_bin_path}")
+if __name__ == "__main__":
+    main()
 
 
